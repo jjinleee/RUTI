0e535ff8dd9ce02679fdb41790bbb42a4be5237c
jjinlee <dlwls6877@naver.com>
2024-07-06 12:22:50 +0900

    init

---
+++ src/main/java/com/hyejin/ruti/RutiApplication.java
@@ -0,0 +1,13 @@
+package com.hyejin.ruti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RutiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RutiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hyejin/ruti/RutiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hyejin.ruti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RutiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
