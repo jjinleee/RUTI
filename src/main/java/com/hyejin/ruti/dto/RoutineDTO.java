14af6ef6aa7dabcaa2cdd0707b99d29bdab4ce70
jjinleee <dlwls6877@naver.com>
2024-08-07 19:33:24 +0900

    feat : routine 기본 구조 구현

---
+++ src/main/java/com/hyejin/ruti/dto/RoutineDTO.java
@@ -0,0 +1,14 @@
+package com.hyejin.ruti.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class RoutineDTO {
+    private String title;
+    private String time;
+    private String duration;
+    private String status;
+    private String frequency;
+}
