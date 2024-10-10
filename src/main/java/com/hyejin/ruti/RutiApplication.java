db14d342aa7282cc301d325ac0fdd38b1d294fa1
jjinleee <dlwls6877@naver.com>
2024-10-10 16:35:55 +0900

    feat : fcm token 저장

--- src/main/java/com/hyejin/ruti/RutiApplication.java
+++ src/main/java/com/hyejin/ruti/RutiApplication.java
@@ -2,8 +2,10 @@ package com.hyejin.ruti;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RutiApplication {
 
 	public static void main(String[] args) {
