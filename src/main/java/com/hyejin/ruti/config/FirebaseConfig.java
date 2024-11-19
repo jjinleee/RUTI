3e5ba32229828e7233c18d60e33f6247404afc90
jjinleee <dlwls6877@naver.com>
2024-11-20 00:19:29 +0900

    fix : firebase 토큰 재발급

--- src/main/java/com/hyejin/ruti/config/FirebaseConfig.java
+++ src/main/java/com/hyejin/ruti/config/FirebaseConfig.java
@@ -1,9 +1,9 @@
 package com.hyejin.ruti.config;
 
-
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,12 +15,21 @@ public class FirebaseConfig {
 
     @Bean
     public FirebaseApp initializeFirebase() throws IOException {
-        FileInputStream serviceAccount = new FileInputStream("src/main/resources/firebase/ruti-fe604-firebase-adminsdk-34x9g-feee600823.json");
+        Dotenv dotenv = Dotenv.configure().load();
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                .build();
+        String firebaseConfigPath = dotenv.get("FIREBASE_CONFIG_PATH");
 
-        return FirebaseApp.initializeApp(options);
+        if (firebaseConfigPath == null) {
+            throw new IllegalStateException("환경 변수 'FIREBASE_CONFIG_PATH'가 설정되지 않았습니다.");
+        }
+
+        try (FileInputStream serviceAccount = new FileInputStream(firebaseConfigPath)) {
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                    .build();
+            return FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            throw new IllegalStateException("Firebase 설정 파일을 읽는 중 오류가 발생했습니다: " + firebaseConfigPath, e);
+        }
     }
 }
